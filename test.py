46fda13e153357c76aa0305d043883df79683546
ttyang <a2617920@hotmail.com>
2019-01-28 11:29:22 +0800

    qwe

---
+++ test.py
@@ -0,0 +1,10 @@
+from sample import sum, sum_only_positive
+
+def test_sum():
+    assert sum(4, 5) == 10
+
+def test_sum_positive_ok():
+    assert sum_only_positive(2, 2) == 4
+
+def test_sum_positive_fail():
+    assert sum_only_positive(-1, 2) is None
